b25967719727e5c3b428429ff020816b29a2e090
João Victor <joaovictorbasilio01@gmail.com>
2026-09-18 10:50:51 -0300

    Implement main function for Lista operations

---
+++ ListaCompletar/main.c
@@ -0,0 +1,43 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include "Lista.h"
+int main()
+{
+    tLista * lista;
+    lista=lista_cria_vazia(15,1,1); //  classif , com repet
+    lista_inclui(lista,2);
+    printf("\n 2");
+    lista_percorre(lista);
+    printf("\n 1 2");
+    lista_inclui(lista,1);
+    lista_percorre(lista);
+    printf("\n 1 2 2 ");
+    lista_inclui(lista,2);
+    lista_percorre(lista);
+    printf("\n 1 1 2 2 ");
+    lista_inclui(lista,1);
+    lista_percorre(lista);
+    printf("\n 1 1 2 2 5");
+    lista_inclui(lista,5);
+    lista_percorre(lista);
+    printf("\n1 1 2 2 5 5 ");
+    lista_inclui(lista,5);
+    lista_percorre(lista);
+    printf("\n1 1 2 2 5 5 5");
+    lista_inclui(lista,5);
+    lista_percorre(lista);
+
+    
+     printf("\n1 1 2 2 ");
+     lista_exclui(lista,5);
+     lista_percorre(lista);
+    
+    return 0;
+}
